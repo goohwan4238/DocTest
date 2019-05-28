0a20a49c3e842c16b83d7f999e50b7404967b85c
leeth0323 <leeth0323@vms-solutions.com>
2019-05-28 20:37:38 +0900

    SideBar Fix

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,7 +38,7 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
+    {doc: 'VMSStyleGuide/GeneralPrinciples', label: 'Docs'},
     {doc: 'doc4', label: 'API'},
     {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
